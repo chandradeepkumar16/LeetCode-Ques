200ec9290cc181b03b5f57b0af0aa180df49af55
Chandradeep Kumar <62211734+chandradeepkumar16@users.noreply.github.com>
2022-07-18 02:14:59 +0530

    Time: 2840 ms (5.13%), Space: 10.4 MB (75.64%) - LeetHub

---
+++ 629-k-inverse-pairs-array/629-k-inverse-pairs-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int kInversePairs(int n, int k) {
+                int dp[n+1][k+1];
+        memset(dp, 0, sizeof(dp));
+        
+        for(int i = 1; i <= n; i++)
+            for(int j = 0; j <= k; j++){
+                //when j = 0(k = 0) then sorted array is the only solution 
+                if(j == 0) dp[i][j] = 1;
+                else
+                    for(int p = 0; p <= min(j, i-1); p++)
+                        dp[i][j] = (dp[i][j] + dp[i-1][j-p])%1000000007;
+            }           
+    return dp[n][k];
+    }
+        
+    
+};
