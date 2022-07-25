7e17eb5b84838766a1f50eefa2026eb794fd0bc2
Chandradeep Kumar <62211734+chandradeepkumar16@users.noreply.github.com>
2022-07-26 03:17:19 +0530

    Time: 412 ms (33.35%), Space: 26.6 MB (38.57%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -17,7 +17,7 @@ public:
             return dp[n][sum]=solve(nums , n-1 ,sum);
         }
         else{
-            return dp[n][sum] = solve(nums , n-1 , sum-nums[n-1]) || solve(nums , n-1 , sum);
+            return dp[n][sum] = solve(nums , n-1 , sum) || solve(nums , n-1 , sum-nums[n-1]);
         }
         // dp[n][sum]=ans;
         // return dp[n][sum];
