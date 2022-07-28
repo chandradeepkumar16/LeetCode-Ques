7fa791cee111c241bdacf7b43cd27117603429b0
Chandradeep Kumar <62211734+chandradeepkumar16@users.noreply.github.com>
2022-07-28 15:26:23 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (46.49%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    int dp[101];
-     int decode(string& s, int idx, int n)
+    // int dp[101];
+     int decode(string& s, int idx, int n , vector<int>&dp)
     {
         if(idx < n && s[idx] == '0') return 0;
         if(idx >= n)
@@ -10,10 +10,13 @@ public:
          if(dp[idx]!=-1) return dp[idx];
         int ways = 0;
         
-        if(s[idx] != '0') ways = decode(s, idx+1, n);
-            
+         //storing the first number
+        if(s[idx] != '0') ways = decode(s, idx+1, n,dp);
+          
+         // storing the second number and checking if it is lying in between range of 11 to 19
+         // or lying between the range of 21 to 26
         if(idx+1 < n && ((s[idx] == '1' && s[idx+1] <= '9') || (s[idx]=='2' && s[idx+1] < '7')))
-           ways += decode(s, idx+2, n);
+           ways += decode(s, idx+2, n,dp);
            
         return dp[idx]=ways;
     }
@@ -21,7 +24,8 @@ public:
     int numDecodings(string s) {
        
         int n = s.size();
-        memset(dp,-1,sizeof dp);
-        return decode(s, 0, n);
+        vector<int> dp(101,-1);
+        // memset(dp,-1,sizeof dp);
+        return decode(s, 0, n , dp);
     }
 };
