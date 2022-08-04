c21746774d9fc90c05b63613c87d01865edf637c
Chandradeep Kumar <62211734+chandradeepkumar16@users.noreply.github.com>
2022-08-04 14:05:10 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (62.81%) - LeetHub

---
+++ 858-mirror-reflection/858-mirror-reflection.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+int gcd(int a,int b)
+{
+  if (b == 0)
+    return a;
+  return gcd(b, a % b);
+}
+    
+    int mirrorReflection(int p, int q) {
+        int lcm = (p*q)/gcd(p, q); 
+        int m = lcm/p;
+        int n = lcm/q;
+        if(m%2==0 && n%2==1) return 0;
+        if(m%2==1 && n%2==1) return 1;
+        if(m%2==1 && n%2==0) return 2;
+        return 0;
+    }
+};
