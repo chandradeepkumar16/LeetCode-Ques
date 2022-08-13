fa91b3a33ae65dc9871cc655080ed9113e25f6e0
Chandradeep Kumar <62211734+chandradeepkumar16@users.noreply.github.com>
2022-08-14 03:06:29 +0530

    Time: 44 ms (28.56%), Space: 7 MB (18.30%) - LeetHub

--- 2193-minimum-number-of-moves-to-make-palindrome/2193-minimum-number-of-moves-to-make-palindrome.cpp
+++ 2193-minimum-number-of-moves-to-make-palindrome/2193-minimum-number-of-moves-to-make-palindrome.cpp
@@ -1,40 +1,56 @@
 class Solution {
 public:
     int minMovesToMakePalindrome(string s) {
-    unordered_map<char, int> unmp;
-    int odd = 0, left = 0, right = s.size() - 1, result = 0;
- 
-    for (char ch : s)
-        unmp[ch]++;
- 
-    for (auto i : unmp)
-        if (i.second % 2 == 1)
-            odd++;
- 
-    if (odd > 1)
-        return -1;
- 
-    while (left < right) {
-        int l = left, r = right;
-        while (s[l] != s[r])
-            r--;
-        if (l == r) {
- 
-            // when we found odd element
-            swap(s[r], s[r + 1]);
-            result++;
-            continue;
+        
+        
+        unordered_map<char , int> um;
+        
+        for(auto i:s){
+            um[i]++;
         }
-        else {
-            // Normal element
-            while (r < right) {
-                swap(s[r], s[r + 1]);
-                result++;
-                r++;
+        int cnt=0;
+        for(auto i:um){
+            if(i.second%2==1){
+                cnt++;
             }
         }
-        left++, right--;
-    }
-    return result;
+        if(cnt>1){
+            return -1;
+        }
+        
+        
+        int left =0 , right = s.size()-1;
+        
+        int res=0;
+        while(left <right){
+            
+            int l= left , r= right;
+            
+            while(s[l]!=s[r]){
+                r--;
+            }
+            
+            if(l==r){
+                swap(s[r] , s[r+1]);
+                res++;
+                continue;
+            }
+            else{
+                
+                while(r<right){
+                    swap(s[r] , s[r+1]);
+                    res++;
+                    r++;
+                }
+                
+            }
+            left++;
+            right--;
+            
+            
+        }
+        
+        return res;
+        
     }
 };
