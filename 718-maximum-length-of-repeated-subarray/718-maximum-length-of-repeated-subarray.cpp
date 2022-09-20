931db096257003d1b7135257ee574a40861e1290
Chandradeep Kumar <62211734+chandradeepkumar16@users.noreply.github.com>
2022-09-20 17:08:53 +0530

    Time: 773 ms (16.74%), Space: 109.2 MB (38.88%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -7,14 +7,7 @@ public:
         
         vector<vector<int>>dp(n+1, vector<int>(m+1,0));
         
-        // dp[0][0]=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(i==0 || j==0){
-                    dp[i][j]=0;
-                }
-            }
-        }
+
         int ans=0;
         for(int i=1;i<n+1;i++){
             for(int j=1;j<m+1;j++){
