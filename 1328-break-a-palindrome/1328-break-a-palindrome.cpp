8fba8145c017b3b0250d4706e2ccaacc492e04f7
Chandradeep Kumar <62211734+chandradeepkumar16@users.noreply.github.com>
2022-10-10 23:45:01 +0530

    Time: 7 ms (10.44%), Space: 6.3 MB (45.53%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string breakPalindrome(string p) {
+        int n=p.size();
+        if(n==1) return "";
+        
+        for(int i=0;i<n/2;i++){
+            if(p[i]!='a'){
+                p[i]='a';
+                return p;
+            }
+        }
+        if(p[n-1]=='a') p[n-1]='b';
+        return p;
+    }
+};
